ea516913b656101a0f608f73846ace8515f8e82d
Aiden Schramm <47539574+AidenSchramm@users.noreply.github.com>
2024-10-03 14:05:48 -0500

    apply metabolic rate calculation

--- resources/views/livewire/metacalc/index.blade.php
+++ resources/views/livewire/metacalc/index.blade.php
@@ -3,6 +3,8 @@
 use Livewire\Volt\Component;
 use Mary\Traits\Toast;
 
+use Illuminate\Http\Request;
+
 
 new class extends Component {
     use Toast;
@@ -11,13 +13,29 @@ new class extends Component {
     public $age = 18;
     public $feet = 5;
     public $inches = 8;
-    public float $rate = 0;
+    public int $rate = 0;
     
-    public $SexOptions = [[ 'name' => "Male", 'id' => 0, 'selected' => true], [ 'name' => "Female", 'id' => 1]];
+    public $sex = [[ 'name' => "Male", 'id' => 0, 'selected' => true], [ 'name' => "Female", 'id' => 1]];
 
-    public function save()
+    public $selected;
+    public function save(Request $request)
     {
-        $this->rate = $this->weight;
+        
+        // Male: BMR = 66.47 + ( 6.24 × weight in pounds ) + ( 12.7 × height in inches ) – ( 6.755 × age in years )
+        // Female: BMR = 655.1 + ( 4.35 × weight in pounds ) + ( 4.7 × height in inches ) - ( 4.676 × age in years )
+
+        error_log($this->selected);
+        
+        if ($this->selected == 0) {
+            $this->rate = 66.47 + ( 6.24 * $this->weight ) + (12.7 * (($this->feet * 12) + $this->inches)) - (6.755 * $this->age);
+        }
+        elseif ($this->selected == 1) {
+            $this->rate = 655.1 + ( 4.35 * $this->weight ) + ( 4.7 * (($this->feet * 12) + $this->inches)) - (4.676 * $this->age);
+        }
+        else {
+            $this->rate = -1;
+        }
+        
     }
 }; ?>
 
@@ -25,14 +43,14 @@ new class extends Component {
     <x-card>
         <x-form wire:submit="save">
             <x-input label="Age" wire:model='age' value="{{ $age }}" type="number"/>
-            <x-radio label="Sex" :options="$SexOptions" selected/>
+            <x-radio label="Sex" :options="$sex" wire:model='selected'/>
             <div style="display: flex">
                 <x-input label="feet" wire:model='feet' value="{{ $feet }}" type="number"/>
                 <x-input label="inches" wire:model='inches' value="{{ $inches }}" type="number"/>
             </div>
             <x-input label="Weight" wire:model='weight' value="{{ $weight }}" type="number" suffix="lbs"/>
             <x-slot:actions>
-                <div style="margin-right: auto; margin-left:1em">{!! $rate !!}</div>
+                <div style="margin-right: auto; margin-left:1em">{!! $rate !!} kcal / day</div>
                 <x-button label="Calculate" class="btn-primary" type="submit" spinner="save" />
             </x-slot:actions>
         </x-form>
